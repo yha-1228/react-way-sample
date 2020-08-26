7341be5563e0ab05dd320c6314e78a2e5f855655
長谷川 裕大 <gssbtsm999@gmail.com>
2020-08-27 08:34:46 +0900

    Updates

---
+++ precache-manifest.f02f3ea71645fb7cd15bf4ff1ae60eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45c83dd7c51a337496531d22d0fee2e",
+    "url": "/react-way-sample/index.html"
+  },
+  {
+    "revision": "2ea503dde646ee31e3dd",
+    "url": "/react-way-sample/static/css/main.1fbae899.chunk.css"
+  },
+  {
+    "revision": "7743148034fa664eaca8",
+    "url": "/react-way-sample/static/js/2.f2650f4f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-way-sample/static/js/2.f2650f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea503dde646ee31e3dd",
+    "url": "/react-way-sample/static/js/main.9dd5bf2d.chunk.js"
+  },
+  {
+    "revision": "95c9dbd256ebbfeeb0a4",
+    "url": "/react-way-sample/static/js/runtime-main.2b1b0a52.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-way-sample/precache-manifest.10095202182c977d6753ccd81cadfec1.js"
+  "/react-way-sample/precache-manifest.f02f3ea71645fb7cd15bf4ff1ae60eb5.js"
 );
 
 self.addEventListener('message', (event) => {
