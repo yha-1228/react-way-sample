ed609abd9dfd05656f147d9aed424d45e7cd1bdf
長谷川 裕大 <gssbtsm999@gmail.com>
2020-08-27 08:35:34 +0900

    CSSの読み込み順を修正

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import FilterableProductTable from './components/FilterableProductTable';
-import './Reset.scss';
-import './App.scss';
+import React from 'react'
+import './Reset.scss'
+import './App.scss'
+import FilterableProductTable from './components/FilterableProductTable'
 
 function App() {
   return (
     <div className="container">
       <FilterableProductTable />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
