d5a40f5768bf008c9eff596c3404c4325700ea2e
長谷川 裕大 <gssbtsm999@gmail.com>
2020-09-19 04:27:49 +0900

    コメントアウト削除

--- src/utils/style-config.ts
+++ src/utils/style-config.ts
@@ -14,27 +14,3 @@ export const fontFamily = [
   '"Segoe UI Emoji"',
   '"Segoe UI Symbol"',
 ].join(',');
-
-// import { createMuiTheme } from '@material-ui/core';
-
-// export const theme = createMuiTheme({
-//   palette: {
-//     primary: {
-//       main: '#496AF6',
-//     },
-//   },
-//   typography: {
-//     fontFamily: [
-//       '-apple-system',
-//       'BlinkMacSystemFont',
-//       '"Segoe UI"',
-//       'Roboto',
-//       '"Helvetica Neue"',
-//       'Arial',
-//       'sans-serif',
-//       '"Apple Color Emoji"',
-//       '"Segoe UI Emoji"',
-//       '"Segoe UI Symbol"',
-//     ].join(','),
-//   },
-// });
