e921849d58e295e5da4a9441db34ca8bad6f0cbd
長谷川 裕大 <gssbtsm999@gmail.com>
2021-02-07 18:03:27 +0900

    U

--- src/ProductList/index.tsx
+++ src/ProductList/index.tsx
@@ -1,108 +1,11 @@
 import React, { useEffect, useReducer } from 'react'
 import Box from '@material-ui/core/Box'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import { Product, Products } from '../types/index'
 import TopBar from './Header'
 import ProductTable from './ProductTable'
 import { PRODUCTS_URL } from '../constants'
 import { wait } from '../functions'
-
-type ProductListState = {
-  error: null | string
-  isLoaded: boolean
-  products: Products
-  filter: { name: string; inStockOnly: boolean }
-  bulkCheckbox: { checked: boolean; indeterminate: boolean }
-  isDeleteLoading: boolean
-}
-
-const initialState = {
-  error: null,
-  isLoaded: false,
-  products: [],
-  filter: { name: '', inStockOnly: false },
-  bulkCheckbox: { checked: false, indeterminate: false },
-  isDeleteLoading: false,
-}
-
-type Action = {
-  type: string
-  payload?: any
-}
-
-function reducer(state: ProductListState, action: Action): ProductListState {
-  switch (action.type) {
-    case 'PENDING': {
-      return { ...state, isLoaded: true }
-    }
-    case 'FULFILLED': {
-      const { result } = action.payload
-      const products = result.map((product: Product) => ({ ...product, checked: false }))
-      return { ...state, isLoaded: true, products }
-    }
-    case 'REJECTED': {
-      const { error } = action.payload
-      return { ...state, isLoaded: true, error }
-    }
-    case 'CHANGE_CHECKBOX': {
-      const { event, id } = action.payload
-
-      const products = state.products.map((product: Product) => ({
-        ...product,
-        checked: product.id === id ? event.target.checked : product.checked,
-      }))
-
-      return {
-        ...state,
-        products,
-        bulkCheckbox: {
-          checked: isSomeChecked(products),
-          indeterminate: isSomeChecked(products) && !isEveryChecked(products),
-        },
-      }
-    }
-    case 'CHANGE_BULK_CHECKBOX': {
-      const { event } = action.payload
-
-      return {
-        ...state,
-        products: state.products.map((product: Product) => ({
-          ...product,
-          checked: event.target.checked,
-        })),
-        bulkCheckbox: { checked: event.target.checked, indeterminate: false },
-      }
-    }
-    case 'PENDING_DELETE': {
-      return { ...state, isDeleteLoading: true }
-    }
-    case 'FULFILLED_DELETE': {
-      const checkedIds = state.products
-        .filter((product: Product) => product.checked)
-        .map((product: Product) => product.id)
-
-      const isNotDeleted = (product: Product) => !checkedIds.includes(product.id)
-
-      return {
-        ...state,
-        products: state.products.filter(isNotDeleted),
-        bulkCheckbox: { checked: false, indeterminate: false },
-        isDeleteLoading: false,
-      }
-    }
-    case 'CHANGE_NAME': {
-      const { name } = action.payload
-      return { ...state, filter: { ...state.filter, name: name } }
-    }
-    case 'TOGGLE_IN_STOCK_ONLY': {
-      const { inStockOnly } = action.payload
-      return { ...state, filter: { ...state.filter, inStockOnly: inStockOnly } }
-    }
-    default: {
-      return state
-    }
-  }
-}
+import reducer, { initialState } from './modules'
 
 export default function ProductList() {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -173,12 +76,4 @@ export default function ProductList() {
       )}
     </>
   )
-}
-
-function isSomeChecked(products: Products) {
-  return products.some((product) => product.checked)
-}
-
-function isEveryChecked(products: Products) {
-  return products.every((product) => product.checked)
 }
---
+++ src/ProductList/modules/index.ts
@@ -0,0 +1,106 @@
+import { Product, Products } from '../../types'
+
+export type ProductListState = {
+  error: null | string
+  isLoaded: boolean
+  products: Products
+  filter: { name: string; inStockOnly: boolean }
+  bulkCheckbox: { checked: boolean; indeterminate: boolean }
+  isDeleteLoading: boolean
+}
+
+export const initialState = {
+  error: null,
+  isLoaded: false,
+  products: [],
+  filter: { name: '', inStockOnly: false },
+  bulkCheckbox: { checked: false, indeterminate: false },
+  isDeleteLoading: false,
+}
+
+export type Action = {
+  type: string
+  payload?: any
+}
+
+export default function reducer(state: ProductListState, action: Action): ProductListState {
+  switch (action.type) {
+    case 'PENDING': {
+      return { ...state, isLoaded: true }
+    }
+    case 'FULFILLED': {
+      const { result } = action.payload
+      const products = result.map((product: Product) => ({ ...product, checked: false }))
+      return { ...state, isLoaded: true, products }
+    }
+    case 'REJECTED': {
+      const { error } = action.payload
+      return { ...state, isLoaded: true, error }
+    }
+    case 'CHANGE_CHECKBOX': {
+      const { event, id } = action.payload
+
+      const products = state.products.map((product: Product) => ({
+        ...product,
+        checked: product.id === id ? event.target.checked : product.checked,
+      }))
+
+      return {
+        ...state,
+        products,
+        bulkCheckbox: {
+          checked: isSomeChecked(products),
+          indeterminate: isSomeChecked(products) && !isEveryChecked(products),
+        },
+      }
+    }
+    case 'CHANGE_BULK_CHECKBOX': {
+      const { event } = action.payload
+
+      return {
+        ...state,
+        products: state.products.map((product: Product) => ({
+          ...product,
+          checked: event.target.checked,
+        })),
+        bulkCheckbox: { checked: event.target.checked, indeterminate: false },
+      }
+    }
+    case 'PENDING_DELETE': {
+      return { ...state, isDeleteLoading: true }
+    }
+    case 'FULFILLED_DELETE': {
+      const checkedIds = state.products
+        .filter((product: Product) => product.checked)
+        .map((product: Product) => product.id)
+
+      const isNotDeleted = (product: Product) => !checkedIds.includes(product.id)
+
+      return {
+        ...state,
+        products: state.products.filter(isNotDeleted),
+        bulkCheckbox: { checked: false, indeterminate: false },
+        isDeleteLoading: false,
+      }
+    }
+    case 'CHANGE_NAME': {
+      const { name } = action.payload
+      return { ...state, filter: { ...state.filter, name: name } }
+    }
+    case 'TOGGLE_IN_STOCK_ONLY': {
+      const { inStockOnly } = action.payload
+      return { ...state, filter: { ...state.filter, inStockOnly: inStockOnly } }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+function isSomeChecked(products: Products) {
+  return products.some((product) => product.checked)
+}
+
+function isEveryChecked(products: Products) {
+  return products.every((product) => product.checked)
+}
