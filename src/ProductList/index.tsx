d600765cbab9fc99c0de65c310fea6f146dc24c6
長谷川 裕大 <gssbtsm999@gmail.com>
2021-02-07 10:10:53 +0900

    U

--- src/ProductList/index.tsx
+++ src/ProductList/index.tsx
@@ -44,7 +44,13 @@ function reducer(state: ProductListState, action: Action) {
       return { ...state, isLoaded: true, error };
     }
     case 'CHANGE_CHECKBOX': {
-      const { products } = action.payload;
+      const { products, event, id } = action.payload;
+
+      products.map((product: Product) => ({
+        ...product,
+        checked: product.id === id ? event.target.checked : product.checked,
+      }));
+
       return {
         ...state,
         products,
